986e2bc0abe52ca48c4ef8aab5b47e9eed8bf04a
Epimeria <61943481+Epimeria@users.noreply.github.com>
2022-04-23 17:14:15 -0500

    added aspect ratio scaling

--- photographyAssist.go
+++ photographyAssist.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
-	"image/jpeg"
+	"image"
 	"image/png"
+	"io/fs"
 	"io/ioutil"
 	"log"
+	"math"
 	"math/rand"
 	"os"
 	"regexp"
@@ -15,13 +17,25 @@ import (
 	"github.com/nfnt/resize"
 )
 
-var imageFormatsRegex, _ = regexp.Compile(`\.((jpg)|(png))$`)
-var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+type ImageConstraints struct {
+	width  int
+	height int
+}
 
-const queueDirectory = "./queue/"
-const downscaledFolderPath = "./processed/localDownscaled/"
+var (
+	imageFormatsRegex, _ = regexp.Compile(`\.((jpg)|(png))$`)
+	letters              = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+)
+
+const (
+	queueFolderPath                = "./queue/"
+	downscaledFolderPath           = "./processed/localDownscaled/"
+	largePictureFolderPath         = "./processed/large/"
+	roughTargetFileSize    float64 = 5000000
+)
 
 func RandomString(n int) string {
+	rand.Seed(time.Now().Unix())
 	s := make([]rune, n)
 	for i := range s {
 		s[i] = letters[rand.Intn(len(letters))]
@@ -34,37 +48,52 @@ func createDirectoriesIfNotExist() {
 	if err != nil {
 		log.Println(err)
 	}
-	err = os.MkdirAll(queueDirectory, os.ModePerm)
+	err = os.MkdirAll(queueFolderPath, os.ModePerm)
+	if err != nil {
+		log.Println(err)
+	}
+	err = os.MkdirAll(largePictureFolderPath, os.ModePerm)
 	if err != nil {
 		log.Println(err)
 	}
 }
 
+func assessMaximumScalingForTargetSize(image image.Image, file fs.FileInfo) ImageConstraints {
+	roughUncompressedSize := (image.Bounds().Dx() * image.Bounds().Dy()) * 6
+
+	var compressionRatio float64 = float64(file.Size()) / float64(roughUncompressedSize)
+	var aspectRatio float64 = float64(image.Bounds().Dy()) / float64((image.Bounds().Dx()))
+	assumedScaledWidth := math.Sqrt(((roughTargetFileSize / compressionRatio) / 6) / aspectRatio)
+
+	return ImageConstraints{int(assumedScaledWidth), 0}
+}
+
 func main() {
+	createDirectoriesIfNotExist()
 
-	queuedFiles, err := ioutil.ReadDir(queueDirectory)
-	if err != nil {
-		log.Fatal(err)
+	queuedFiles, err := ioutil.ReadDir(queueFolderPath)
+	if err != nil || len(queuedFiles) == 0 {
+		log.Fatal("Failed to run, queue empty")
 	}
-
-	fmt.Println(queuedFiles)
 
 	for iterationNumber, file := range queuedFiles {
 
 		if imageFormatsRegex.MatchString(file.Name()) {
-			file, err := os.Open(file.Name())
+			fileData, err := os.Open(fmt.Sprintf("%s%s", queueFolderPath, file.Name()))
 			if err != nil {
 				log.Fatal(err)
 			}
-			image, err := jpeg.Decode(file)
+			image, err := png.Decode(fileData)
 			if err != nil {
 				log.Fatal(err)
 			}
-			file.Close()
+			fileData.Close()
 
-			// Target size: 5mb
-			resizedImage := resize.Resize(1000, 0, image, resize.Bicubic)
-			fileName := fmt.Sprintf("film-%s-%s-%x", strconv.Itoa(iterationNumber), time.Now().Format("yyyy-MM-dd"), RandomString(6))
+			constraints := assessMaximumScalingForTargetSize(image, file)
+			fmt.Sprintln(constraints)
+
+			resizedImage := resize.Resize(uint(constraints.width), uint(constraints.height), image, resize.Lanczos3)
+			fileName := fmt.Sprintf("film-%s-%s-%x.png", strconv.Itoa(iterationNumber), time.Now().Format("2006-02-01"), RandomString(6))
 
 			output, err := os.Create(fmt.Sprintf("%s%s", downscaledFolderPath, fileName))
 			if err != nil {
@@ -73,26 +102,11 @@ func main() {
 			defer output.Close()
 
 			png.Encode(output, resizedImage)
+			fmt.Println(file.Name())
+			os.Rename(fmt.Sprintf("%s%s", queueFolderPath, file.Name()), fmt.Sprintf("%slarge-%s", largePictureFolderPath, fileName))
 		} else {
 			log.Fatalf("Failed to parse file: %v, please correct and rerun", file.Name())
 		}
-
-		fmt.Println(file.Name(), file.IsDir(), file.Size())
 	}
 
-	/*
-
-		// resize to width 1000 using Lanczos resampling
-		// and preserve aspect ratio
-		m := resize.Resize(1000, 0, img, resize.Lanczos3)
-
-		out, err := os.Create("test_resized.jpg")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer out.Close()
-
-		// write new image to file
-		jpeg.Encode(out, m, nil)
-	*/
 }
